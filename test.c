ff510cbd65499f8897621c4c49a1d9a496de0fef
lxl-git05 <XinLiu051024@outlook.com>
2025-09-21 21:40:13 +0800

    添加test.c的修改

--- test.c
+++ test.c
@@ -1,11 +1,8 @@
-#include <stdio.h>  
-#include "max.h"    // 引用自定义的头文件
+#include <stdio.h>     // 引用自定义的头文件
 int main() 
 {
     int a = 10;
     int b = 20;
-    int c = max(a, b);
-    printf("max(%d, %d) = %d\n", a, b, c);
     printf("Hello, world!\n");
     return 0;
 }
